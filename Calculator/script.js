7d5149feda0654e2647dea4e001b33dab2f8d75e
Paresh8797 <104883898+Paresh8797@users.noreply.github.com>
2024-03-19 18:24:08 +0530

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,28 @@
+// console.log("helo");
+let input = document.getElementById('inp');
+let buttons = document.querySelectorAll('button');
+
+let str = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == "=") {
+            str = eval(str);
+            input.value = str;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            str = "";
+            input.value = str;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            str =str.substring(0,str.length-1);
+            input.value = str;
+
+        }
+        else {
+            str += e.target.innerHTML;
+            input.value = str;
+        }
+
+    })
+})
